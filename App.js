84d52d4bb072270170f7ca803a6eb8bc64244eae
Lok Sai Palli <loksai.palli@engro.io>
2024-10-01 19:54:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+
+
+
+const element= React.createElement('p',{},React.createElement('span',{},'span element...!'));
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(element);
